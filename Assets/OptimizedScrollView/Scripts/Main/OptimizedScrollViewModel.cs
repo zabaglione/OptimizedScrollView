8a95cd201e2158500f3c485d8bcb72662cbd1d95
zabaglione <z.zabaglione@gmail.com>
2024-09-16 15:34:41 +0900

    namespaceを修正

--- Assets/OptimizedScrollView/Scripts/Main/OptimizedScrollViewModel.cs
+++ Assets/OptimizedScrollView/Scripts/Main/OptimizedScrollViewModel.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace zabaglione.OptimizedScrollView
+namespace Zabaglione.OptimizedScrollView
 {
     /// <summary>
     /// インターフェース：ScrollViewのModel
--- Assets/OptimizedScrollView/Scripts/Main/OptimizedScrollViewView.cs
+++ Assets/OptimizedScrollView/Scripts/Main/OptimizedScrollViewView.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace zabaglione.OptimizedScrollView
+namespace Zabaglione.OptimizedScrollView
 {
     public class OptimizedScrollViewView : MonoBehaviour
     {
